713b7ab8d1e9910797c411c1583678227bfecb6b
HirotakeIto <hirotake_ito@hirotakoMacBook>
2018-11-14 14:27:25 +0900

    adding cls like sklearn regression

--- peer_fixed_effect/core.py
+++ peer_fixed_effect/core.py
@@ -1,12 +1,39 @@
 # -*- coding: utf-8 -*-
+from sklearn.base import BaseEstimator
 from . import helpers
+import pandas as pd
+import numpy as np
+from typing import Union
 
-def get_hmm():
-    """Get a thought."""
-    return 'hmmm...'
 
+class PeerFixedEffectRegression(BaseEstimator):
+    gammma = None
+    alpha = None
+    ids = None
+    time = None
 
-def hmm():
-    """Contemplation..."""
-    if helpers.get_answer():
-        print(get_hmm())
+    def __init__(self, effect='static', max_iteration=1000, seed=None):
+        super().__init__()
+        self.effect = effect
+        self.max_iteration = max_iteration
+        if seed:
+            np.random.seed(seed)
+
+    def fit(self,
+            x: Union(np.array, pd.DataFrame),
+            y: Union(np.array, pd.DataFrame),
+            group: Union(np.array, pd.DataFrame),
+            ids: Union(np.array, pd.DataFrame),
+            times: Union(np.array, pd.DataFrame)):
+        """
+
+        :param x:
+        :param y:
+        :param ids:
+        :param times:
+        :return:
+        """
+        pass
+
+    def predict(self):
+        pass

412cd840799411e5bef7a3bc3645ce89949eb13f
HirotakeIto <hirotake_ito@hirotakoMacBook>
2018-12-23 02:35:53 +0900

    Wild Bootstrapを追加します

--- peer_fixed_effect/core.py
+++ peer_fixed_effect/core.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from numpy import random, unique
-from functools import reduce
+from numpy import random, unique, multiply, array
 from sklearn.base import BaseEstimator
 from .structure import StaticPeerFixedEffectStructureMixin, CumulativePeerFixedEffectStructureMixin
 
@@ -16,6 +15,10 @@ class PeerEffectEstimator:
     @property
     def parameters(self):
         return self.gamma0, self.alpha_it0, self.alpha_it
+
+    @property
+    def parameters_dict(self):
+        return {'gamma0': self.gamma0, 'alpha_it0':self.alpha_it0, 'alpha_it':self.alpha_it}
 
     @property
     def peer_effect(self):
@@ -34,12 +37,17 @@ class PeerEffectEstimator:
         else:
             if isinstance(init, dict) is False:
                 raise ValueError('please dict')
-            if reduce(lambda x, y: x & y, map(lambda x: x in ['gamma0', 'alpha_it0'], list(init.keys()))) is False:
-                raise ValueError('init must have gamma0, alpha_it0 keys')
+            # if reduce(lambda x, y: x & y, map(lambda x: x in ['gamma0', 'alpha_it0'], list(init.keys()))) is False:
+            for x in ['gamma0', 'alpha_it0']:
+                if x not in init.keys():
+                    raise ValueError('init must have {x} keys'.format(x=x))
             self.gamma0 = init['gamma0']
             self.alpha_it0 = init['alpha_it0']
 
     def fit(self, **argv):
+        raise NotImplementedError('You must call abstract method.')
+
+    def predict(self, **argv):
         raise NotImplementedError('You must call abstract method.')
 
 
@@ -75,6 +83,17 @@ class StaticPeerFixedEffectEstimator(PeerEffectEstimator, StaticPeerFixedEffectS
                     print("{q}th iteration: estimated gamma is {gamma0:.4f}".format(q=iteration, gamma0=gamma0))
                 gamma0_prime = gamma0
         return gamma0_prime, alpha_it0, alpha_it
+
+    def predict(self, reg_cls):
+        return self._predict(
+            time_it=reg_cls.time_it, group_it=reg_cls.group_it,
+            gamma0=reg_cls.gamma0, alpha_it0=reg_cls.alpha_it0
+        )
+
+    def _predict(self, time_it, group_it, gamma0, alpha_it0):
+        alpha_it = self.get_alpha_it(alpha_it0)
+        mean_alpha_jt = self.get_mean_alpha_jt(group_it, time_it, alpha_it)
+        return alpha_it + gamma0 * mean_alpha_jt
 
 
 class CumulativePeerFixedEffectEstimator(PeerEffectEstimator, CumulativePeerFixedEffectStructureMixin):
@@ -130,14 +149,34 @@ class PeerFixedEffectRegression(BaseEstimator):
         self.init = None
         self.estimator = self.estimator_mapper[effect](
             max_iteration=max_iteration, seed=seed, gamma_boundry=gamma_boundry, verbose=verbose)
+        self.peer_effect_sample = None
+
+    @property
+    def peer_effect_parametor(self):
+        if self.peer_effect_sample is None:
+            return self.estimator.peer_effect
+        if self.peer_effect_sample is not None:
+            return array(self.peer_effect_sample).mean(), array(self.peer_effect_sample).std()
 
     def set_attribute(self, **params):
         for key, value in params.items():
             self.__setattr__(key, value)
 
-    def fit(self, id_it, time_it, group_it, y_it, delta_it=None, init=None):
+    def fit(self, id_it, time_it, group_it, y_it, delta_it=None, init=None, bootstrap=False):
         self.set_attribute(id_it=id_it, time_it=time_it, group_it=group_it, y_it=y_it, delta_it=delta_it, init=init)
-        self.estimator.fit(self)
+        self.estimator.fit(reg_cls=self)
+        self.set_attribute(**self.estimator.parameters_dict)
+        if bootstrap is True:
+            self.set_attribute(init=self.estimator.parameters_dict)
+            peer_effect_value = []
+            for i in range(100):
+                resid_it = y_it - self.predict()
+                y_it_updated = y_it + multiply(resid_it, random.standard_normal(size=resid_it.shape))
+                self.set_attribute(y_it=y_it_updated)
+                self.estimator.fit(reg_cls=self)
+                peer_effect_value.append(self.estimator.peer_effect)
+                self.set_attribute(**self.estimator.parameters_dict)
+            self.peer_effect_sample = peer_effect_value
 
     def predict(self):
-        pass
+        return self.estimator.predict(reg_cls=self)
